3f384beda4acf61926fb25fde814d56131f41078
Izzy salley <isalley@Izzys-MacBook-Air.local>
2018-12-04 12:21:46 -0500

    update

--- googlenet_minitrain.m
+++ googlenet_minitrain.m
@@ -123,8 +123,8 @@ function stop=savetrainingplot(info)
 stop=false;  %prevents this function from ending trainNetwork prematurely
 if info.State=='done'   %check if all iterations have completed
 % if true
-        fig = findall(groot, 'Type', 'Figure')
-        saveas(fig(2),'TrainingProgress.png')  % save figure as .png, you can change this
+        fig = findall(groot, 'Type', 'Figure');
+        saveas(fig,'TrainingProgress.png')  % save figure as .png, you can change this
 end
 end
 
